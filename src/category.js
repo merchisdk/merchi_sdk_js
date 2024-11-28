930b75f94572824b72141b46f52688bc0fcf0d4a
James Cool <jamescool@Jamess-MacBook-Pro.local>
2024-11-28 16:06:34 +1100

    add descendant

--- src/category.js
+++ src/category.js
@@ -14,6 +14,7 @@ export function Category() {
     addPropertyTo(this, 'showDashboard');
     addPropertyTo(this, 'showPublic');
     addPropertyTo(this, 'showPublicSupplierResell');
+    addPropertyTo(this, 'descendant');
     addPropertyTo(this, 'image', MerchiFile);
     addPropertyTo(this, 'domain', Domain);
     addPropertyTo(this, 'subcategories', Category);
